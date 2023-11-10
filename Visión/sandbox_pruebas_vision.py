34671f77350b31beb6c14f79f9c1cd778f9a13b0
ajic21 <antoniojavier.i.c@opendeusto.es>
2023-11-10 19:36:54 +0100

    Métodos unificados en vision_tablero, métodos añadidos, sandbox_pruebas_vision para pruebas y tablero.png

---
+++ Visión/sandbox_pruebas_vision.py
@@ -0,0 +1,51 @@
+import cv2
+import numpy as np
+import vision_tablero as vt
+
+# Cargar una imagen o capturar desde la cámara
+image = cv2.imread('Imagenes/tablero.png')  # Cambia la ruta de la imagen según tus necesidades
+rectangulo_tablero = vt.obtener_rectangulo_tablero(image)
+mascara_a, mascara_r = vt.obtener_mascaras_colores(image)
+
+# Bloque que obtiene las fichas azules
+fichas_azules = vt.obtener_fichas_color(image, mascara_a)
+if fichas_azules is not None:
+    for i in fichas_azules[0, :]:
+        # dibuja en rosa la circunferencia de todas las fichas azules
+        cv2.circle(image, (i[0], i[1]), i[2], (255, 0, 255), 2)
+        # si la ficha esta dentro del tablero marca el centro en amarillo
+        if vt.dentro(i, rectangulo_tablero):
+            cv2.circle(image, (i[0], i[1]), 2, (0, 255, 255), 3)
+
+    ficha_libre = vt.obtener_ficha_libre(fichas_azules, rectangulo_tablero)
+    # si hay una ficha libre marca el centro en verde
+    if ficha_libre is not None:
+        cv2.circle(image, (ficha_libre[0], ficha_libre[1]), 2, (0, 255, 0), 3)
+
+# Bloque que contiene las fichas rojas
+fichas_rojas = vt.obtener_fichas_color(image, mascara_r)
+if fichas_rojas is not None:
+    for i in fichas_rojas[0, :]:
+        # dibuja en negro la circunferencia de todas las fichas rojas
+        cv2.circle(image, (i[0], i[1]), i[2], (0, 0, 0), 2)
+        # si la ficha está dentro del tablero, marca el centro en amarillo
+        if vt.dentro(i, rectangulo_tablero):
+            cv2.circle(image, (i[0], i[1]), 2, (0, 255, 255), 3)
+
+    ficha_libre = vt.obtener_ficha_libre(fichas_rojas, rectangulo_tablero)
+    # si hay una ficha libre, marca el centro en verde
+    if ficha_libre is not None:
+        cv2.circle(image, (ficha_libre[0], ficha_libre[1]), 2, (0, 255, 0), 3)
+
+
+# Obtiene el estado del tablero codificado en un entero decimal
+estado = vt.obtener_estado_tablero(image)
+print(estado)
+
+#obtiene el estado real del tablero como una secuencia de ceros, unos y doses
+estado_descodificado = vt.base3(estado)
+print(estado_descodificado)
+
+cv2.imshow('Sandbox de Pruebas de Vision', image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
---
+++ Visión/vision_tablero.py
@@ -0,0 +1,115 @@
+import cv2
+import numpy as np
+
+# Umbral superior e inferior para cada color
+umbral_inferior_azul = np.array([90, 50, 50])
+umbral_superior_azul = np.array([130, 255, 255])
+umbral_inferior_rojo1 = np.array([0, 100, 100])
+umbral_superior_rojo1 = np.array([10, 255, 255])
+umbral_inferior_rojo2 = np.array([160, 100, 100])
+umbral_superior_rojo2 = np.array([180, 255, 255])
+
+# Devuelve máscaras para cada color
+def obtener_mascaras_colores(imagen):
+    hsv = cv2.cvtColor(imagen, cv2.COLOR_BGR2HSV)
+    mascara_azul = cv2.inRange(hsv, umbral_inferior_azul, umbral_superior_azul)
+    mascara_rojo1 = cv2.inRange(hsv, umbral_inferior_rojo1, umbral_superior_rojo1)
+    mascara_rojo2 = cv2.inRange(hsv, umbral_inferior_rojo2, umbral_superior_rojo2)
+    mascara_rojo = mascara_rojo1 + mascara_rojo2
+
+    return mascara_azul, mascara_rojo
+
+# Devuelve un array de fichas (circulares) detectadas en 'imagen' de un color contenido en 'mascara'
+def obtener_fichas_color(imagen, mascara) -> []:
+    fichas = cv2.bitwise_and(imagen, imagen, mask = mascara)
+    imagen_gris = cv2.cvtColor(fichas, cv2.COLOR_BGR2GRAY)
+    imagen_gris = cv2.medianBlur(imagen_gris, 5)
+    fichas = cv2.HoughCircles(imagen_gris, cv2.HOUGH_GRADIENT_ALT, 1, 20, param1=100, param2=0, minRadius=0, maxRadius=0)
+
+    if fichas is not None:
+        fichas = np.uint16(np.around(fichas))
+
+    return fichas
+
+# Devuelve los parámetros x, y, w, h que delimitan el rectángulo del tablero
+def obtener_rectangulo_tablero(imagen):
+    imagen_gris = cv2.cvtColor(imagen, cv2.COLOR_BGR2GRAY)
+    imagen_desenfocada = cv2.GaussianBlur(imagen_gris, (5, 5), 0)
+    _, imagen_umbralizada = cv2.threshold(imagen_desenfocada, 50, 255, cv2.THRESH_BINARY)
+    imagen_bordes = cv2.Canny(imagen_umbralizada, 20, 80)
+    
+    contornos, _ = cv2.findContours(imagen_bordes, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    contorno_tablero = max(contornos, key=cv2.contourArea)
+
+    return cv2.boundingRect(contorno_tablero)
+
+# Devuelve las casillas con los parámetros x, y, w, h que delimitan el rectángulo del tablero
+def obtener_rectangulos_casillas(rectangulo_tablero):
+    ancho_casilla = int(rectangulo_tablero[2]/3)
+    alto_casilla = int(rectangulo_tablero[3]/3)
+    rectangulos_casillas = []
+
+    for i in range(3):
+        for j in range(3):
+            rectangulos_casillas.append((rectangulo_tablero[0] + j*ancho_casilla, 
+                                         rectangulo_tablero[1] + i*alto_casilla, ancho_casilla, alto_casilla))
+
+    return rectangulos_casillas
+
+# Devuelve True si el centro de 'ficha' está dentro de 'rectangulo' (x, y, w, h)
+def dentro(ficha, rectangulo) -> bool:
+    centro_ficha_x = ficha[0]
+    centro_ficha_y = ficha[1]
+    radio_ficha = ficha[2]-5
+
+    if  rectangulo[0] < (centro_ficha_x-radio_ficha) and (centro_ficha_x+radio_ficha) < (rectangulo[0]+rectangulo[2]) and \
+        rectangulo[1] < (centro_ficha_y-radio_ficha) and (centro_ficha_y+radio_ficha) < (rectangulo[1]+rectangulo[3]):
+        return True
+    else: 
+        return False
+
+# Devuelve la primera 'ficha' que no está dentro de 'rectangulo_tablero'
+def obtener_ficha_libre(fichas, rectangulo_tablero):
+    for i in fichas[0, :]:
+        if not dentro(i, rectangulo_tablero):
+            return i
+
+# Devuelve un entero que transformado a base 3 representa el estado del tablero
+def obtener_estado_tablero(imagen) -> int:
+    
+    tablero = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    estado = 0
+
+    rectangulo_tablero = obtener_rectangulo_tablero(imagen)
+    rectangulos_casillas = obtener_rectangulos_casillas(rectangulo_tablero)
+
+    mascara_azul, mascara_rojo = obtener_mascaras_colores(imagen)
+
+    fichas_azules = obtener_fichas_color(imagen, mascara_azul)
+    fichas_rojas = obtener_fichas_color(imagen, mascara_rojo)
+
+    for i in range(9):
+        for j in fichas_azules[0, :]:
+            if dentro(j, rectangulos_casillas[i]):
+                tablero[i] = 1
+                estado += 1*(3**i)
+        for k in fichas_rojas[0, :]:
+            if dentro(k, rectangulos_casillas[i]):
+                tablero[i] = 2
+                estado += 2*(3**i)
+                
+    return estado
+
+# Devuelve una cadena de 9 caracteres (0, 1, 2) representando el estado del tablero
+def base3(numero) -> str:
+    resto = ""
+    for i in range(9):
+        if numero != 0:
+            cociente = int(numero / 3)
+            resto += str(numero % 3)
+            numero = cociente
+        else:
+            resto += "0"
+    
+    return resto
+    
